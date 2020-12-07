ae8841bd9874f54b4bce0c81f38c9186eaefc71a
Kyle Guerrero <kyleguerrero85@gmail.com>
2020-12-06 20:44:06 -0600

    feat: filled in base content
    filled in base content such as the about me section along
    with the projects section

--- src/mock/data.js
+++ src/mock/data.js
@@ -10,17 +10,20 @@ export const headData = {
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  name: 'Kyle Guerrero',
+  subtitle: "I'm a Full Stack Web Developer",
+  cta: 'Learn More',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
+  img: 'me.jpeg',
+  paragraphOne:
+    'I have been working towards becoming a Web Developer since 2018. Professionally, I have held a number of different roles including: Installation Supervisor, Maintenance Technician, and Systems Analyst.',
+  paragraphTwo:
+    'I attended and completed Lambda School as a stepping stone in becoming a Web Developer.',
+  paragraphThree:
+    'During my time at Lambda I gained experience in working with a wide range of technology including but not limited to JavaScript, React, Redux, Node/Express, Python, Flask, Django, GraphQL, HTML & CSS, CSS Preprocessors, Jest, and React Testing Library.',
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
@@ -28,30 +31,33 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'ncov19_thumbnail.png',
+    title: 'Covid Dash',
+    info:
+      'Covid dash allows users to view current up to date stats and news on the novel corona virus. Users can select their state and view data in their specific county and all other counties in the state.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://ncov19.us/',
+    repo: 'https://github.com/ncov19-us', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'rick-n-morty_thumbnail.png',
+    title: 'Rick And Morty Fan App',
+    info:
+      'The Rick And Morty fan site allows users to view information about the show Rick and Morty. The app displays information about individual characters, different locations that the show takes place in, along with episodes and details about each episode.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://rick-n-morty-fan-page.netlify.app/',
+    repo: 'https://github.com/AceMouty/Sprint-Challenge-Single-Page-Apps', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
+    img: 'twitoff_thumbnail.png',
+    title: 'TwitOff',
+    info:
+      'With TiwtOff users are able to select two twitter users, write a tweet and see who is more likely to make a similar tweet. The prediction is made with a machine learning model. Users can also add and remove twitter users from the app, along with viewing tweets from the current twitter users in the app.',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'https://twitoff.com/',
+    repo: 'https://github.com/hurshd0/TwitOff', // if no repo, the button will not show up
   },
 ];
 
