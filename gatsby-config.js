1201698a00fb10312beabf8feadd1b26a2c9a8b9
Kyle Guerrero <kyleguerrero85@gmail.com>
2020-12-08 22:30:50 -0600

    feat: connected form to formspree
    connected the form to form spree using fetch
    
    style: added stying to the submit button along with basic styling to the success message
    config: added in dotenv for using env vars in the app
    
    created slices of state to hold form information

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,8 @@
+
+require("dotenv").config({
+  path: '.env',
+})
+
 module.exports = {
   plugins: [
     `gatsby-plugin-sass`,
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -5,37 +5,66 @@ import Col from 'react-bootstrap/Col'
 
 function ContactForm() {
 
-    const [firstName, setFirstName] = React.useState("");
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [message, setMessage] = React.useState("");
+    const [phone, setPhone] = React.useState("");
+    const [success, setSuccess] = React.useState(false);
 
     function changeHandler(e) {
-        console.log(e.target.name)
-        setFirstName(e.target.value)
-        console.log("value changed")
+        if (e.target.name === "name") {
+            setName(e.target.value)
+        } else if (e.target.name === "email") {
+            setEmail(e.target.value)
+        } else if (e.target.name === "phone") {
+            setPhone(e.target.value)
+        } else if (e.target.name === "message") {
+            setMessage(e.target.value)
+        }
+
+    }
+
+    function submitHandler(e) {
+        e.preventDefault();
+        const form = e.target;
+        const data = {
+            name,
+            email,
+            phone,
+            message
+        }
+
+        const config = {
+            method: form.method,
+            headers: { 'Content-Type': 'application/json;charset=utf-8' },
+            body: JSON.stringify(data)
+        }
+
+        fetch(form.action, config)
+            .then(res => res.json())
+            .then(data => {
+                if (data.ok) {
+                    setSuccess(true);
+                }
+            })
+
+        e.target.reset();
     }
 
     return (
-        <Form>
+        <Form as="form" onSubmit={submitHandler} action={process.env.GATSBY_FORMAPI} method="POST">
             <Row>
                 <Col md={6} sm={12} className="contact-form-input-container d-flex flex-column justify-content-between">
-                    <Form.Control className="contact-form__input" placeholder="First name*" value={firstName} onChange={changeHandler} name="firstName" />
-                    <Form.Control className="contact-form__input my-4 my-md-0" placeholder="Last name*" />
-                    <Form.Control className="contact-form__input" placeholder="Phone*" />
+                    <Form.Control className="contact-form__input" placeholder="Name*" onChange={changeHandler} name="name" />
+                    <Form.Control className="contact-form__input my-4 my-md-0" placeholder="Email*" onChange={changeHandler} name="email" />
+                    <Form.Control className="contact-form__input" placeholder="Phone*" onChange={changeHandler} name="phone" />
                 </Col>
                 <Col md={6} sm={12} className="mt-4 mt-md-0">
-                    <Form.Control className="contact-form__message" as='textarea' placeholder="Message*" />
+                    <Form.Control className="contact-form__message" as='textarea' placeholder="Message*" onChange={changeHandler} name="message" />
                 </Col>
             </Row>
-            <Row>
-                <Col className="mt-5">
-                    <a
-                        href="#"
-                        rel="noopener noreferrer"
-                        className="cta-btn form-btn"
-                    >
-                        Send Message
-                    </a>
-                </Col>
-            </Row>
+            {!success && <button rel="noopener noreferrer" className="cta-btn form-btn mt-5" type="submit">Send Message</button>}
+            {success && <p className="mt-5 form-success">Thank You!</p>}
         </Form>
     )
 }
